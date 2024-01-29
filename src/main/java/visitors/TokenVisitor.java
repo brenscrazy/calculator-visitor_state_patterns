10057293fe7e258a1f9b206f1e6efc71668ca8eb
brenscrazy <32305955+brenscrazy@users.noreply.github.com>
2024-01-29 18:49:56 +0500

    Update TokenVisitor.java

--- src/main/java/visitors/TokenVisitor.java
+++ src/main/java/visitors/TokenVisitor.java
@@ -5,7 +5,6 @@ import tokens.*;
 public interface TokenVisitor {
 
     void visit(NumberToken token);
-
     void visit(LeftParenToken token);
     void visit(RightParenToken token);
     void visit(OperationToken token);
